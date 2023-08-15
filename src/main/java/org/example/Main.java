986b840214918b8b7c783adf23dcffe4cd640fde
Ali Shahidi <alishahidi1376@gmail.com>
2023-08-15 19:01:10 +0330

    add bank class and read files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
+import org.example.bank.Bank;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Bank bank = new Bank();
     }
 }
--- src/main/java/org/example/bank/Bank.java
+++ src/main/java/org/example/bank/Bank.java
@@ -1,4 +1,39 @@
 package org.example.bank;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.example.utility.Helpers;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class Bank {
+    private List<Account> accounts;
+    private List<Transaction> transactions;
+
+    public Bank() {
+        List<String> accountLines = Helpers.readLines("/accounts.csv");
+        accounts = accountLines.stream()
+                .map(account -> account.split(",", 3))
+                .map(account -> {
+                    Integer id = Integer.parseInt(account[0]);
+                    String name = account[1];
+                    long amount = Long.parseLong(account[2].replaceAll(",", ""));
+                    return new Account(id, name, amount, amount);
+                })
+                .toList();
+
+        List<String> transactionLines = Helpers.readLines("/transactions.csv");
+        transactions = transactionLines.stream()
+                .map(account -> account.split(",", 3))
+                .map(account -> {
+                    Integer from = Integer.parseInt(account[0]);
+                    Integer to = Integer.parseInt(account[1]);
+                    long amount = Long.parseLong(account[2].replaceAll("\"", "").replaceAll(",", ""));
+                    return new Transaction(from, to, amount);
+                })
+                .toList();
+    }
+
 }
